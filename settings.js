aa9b41fca81e4ba1ba4bc64dc1d991b20493b8d0
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2025-02-28 18:35:44 +0100

    Add morganStanleyAtWork to settings.

--- settings.js
+++ settings.js
@@ -1,17 +1,21 @@
 function save_options() {
     var ibkr = document.getElementById('ibkr').checked;
+    var morganStanleyAtWork = document.getElementById('morganStanleyAtWork').checked;
     chrome.storage.sync.set({
         ibkr: ibkr,
+        morganStanleyAtWork: morganStanleyAtWork,
     });
 }
 document.getElementById('ibkr').addEventListener('click', save_options);
+document.getElementById('morganStanleyAtWork').addEventListener('click', save_options);
 
 function restore_options() {
     chrome.storage.sync.get({
         ibkr: true,
+		morganStanleyAtWork: true,
     }, function (items) {
-        console.log("restore options");
         document.getElementById('ibkr').checked = items.ibkr;
+        document.getElementById('morganStanleyAtWork').checked = items.morganStanleyAtWork;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
